7bd0b47a388238a93fccaa713e4e04d0f009e294
himansu1990 <himansu.happy@gmail.com>
2024-08-27 10:49:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let body = document.querySelector('body')
 let cursor = document.querySelector('.cursor')
 let slideImage = document.querySelectorAll('.slide-image')
-
+let menu = document.querySelector('.menu')
+let menuDet = document.querySelector('.men-det')
 
 body.addEventListener('mousemove', function(dets){
      cursor.style.left = dets.x + "px"
@@ -20,3 +21,21 @@ dets.addEventListener('mouseleave', function(){
 
 })
 
+//Create click event for menu
+let siteMenu = 0
+
+menu.addEventListener('click', function(){
+    if(siteMenu == 0){
+        menuDet.style.display = 'block'
+        menuDet.style.marginTop = '-8em'
+        // menu.style.marginTop = '-0.05em'
+        menu.innerHTML = 'Close'
+        siteMenu = 1
+    }
+    else if(siteMenu == 1){
+        menuDet.style.display = 'none'
+        menu.innerHTML = 'Menu'
+        siteMenu = 0
+    }
+
+})
